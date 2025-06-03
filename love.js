ae13df2f16ed549e8ba8c352b56283b8f6cdd732
zimam78 <zmam56800@gmail.com>
2025-06-03 11:26:52 +0700

    Add files via upload

---
+++ love.js
@@ -0,0 +1,9 @@
+const envelope = document.getElementById("envelope");
+
+function openEnvelope() {
+    envelope.classList.add("open");
+}
+
+function closeEnvelope() {
+    envelope.classList.remove("open");
+}
